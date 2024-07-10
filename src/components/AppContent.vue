8cea968fff15218d9daac61eaa0d395d43390765
Francescosommellaa <franc.sommella@gmail.com>
2024-07-10 16:44:22 +0200

    Create: basic structure for card insertion

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -2,13 +2,66 @@
 
 export default {
     name: "AppContent",
+
+    data() {
+
+    }
 }
 
 </script>
 
 <template>
+
+    <div class="container">
+        <div class="container-card">
+            <div class="counter">
+                <span>Trovate N carte</span>
+            </div>
+            <div class="card-container">
+                <div class="card">
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <style lang="scss" scoped>
+
+.container {
+    background-color: #D48F38;
+    height: calc(100vh - 60px);
+    width: 100vw;
+    padding: 1em 3em 0 3em;
+    overflow: auto;
+
+    .container-card {
+        background-color: #fff;
+        height: 100%;
+        width: 100%;
+        padding: 2em;
+
+        .counter {
+            background-color: #000;
+            color: white;
+            padding: 0.8em;
+        }
+
+        .card-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: start;
+            flex-wrap: wrap;
+            gap: 20px;
+
+            .card {
+                background-color: #D48F38;
+                width: calc(100% / 5 - 20px);
+            }
+        }
+    }
+}
 
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,10 +7,26 @@ export default {
 </script>
 
 <template>
+
+<div>
+    <img src="https://th.bing.com/th/id/R.fc7e0dc9e6aa7098fad7d4c6cf12b4f9?rik=WDGP5hthzVRcXQ&riu=http%3a%2f%2fimg2.wikia.nocookie.net%2f__cb20131129140054%2fyugioh%2fimages%2f6%2f61%2fYugioh_Logo.png&ehk=rLjj1tbjHmMa1ijemgUXLtPrujJmOR0Zs8OZFj5dIag%3d&risl=&pid=ImgRaw&r=0" alt="">
+    <h1>Yu-Gi-Oh Api</h1>
+</div>
+
+
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/mixins' as * ;
 @use '../styles/partials/variables' as * ;
 
+
+div {
+    @include center();
+    height: 60px;
+
+    img {
+        height: 40px;
+    }
+}
 </style>
