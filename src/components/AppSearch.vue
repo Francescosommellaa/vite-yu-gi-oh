e443c79ee0d77f47fbc68890b9c34fa48f8b684c
Francescosommellaa <franc.sommella@gmail.com>
2024-07-12 09:08:52 +0200

    create: search select component

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -3,19 +3,24 @@ export default {
     name: "AppSearch",
     
     data() {
-        return {
-            store,
-        }
     }
 }
 </script>
 
 <template>
-    <div>
-        
+    <div class="container">
+        <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+            <option selected>Scegli l'archetipo</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
+    .container {
+        background-color: #D48F38;
+        padding: 1em 3em;
+    }
 </style>
